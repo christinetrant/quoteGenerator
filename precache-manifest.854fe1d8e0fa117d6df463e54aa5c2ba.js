25707f47e56c0d9f00d4714268b7ef5b35d960a9
Christine Trant <christinetrant@gmail.com>
2020-08-31 17:28:05 +0100

    Updates

---
+++ precache-manifest.854fe1d8e0fa117d6df463e54aa5c2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35857d3458a9ec152ae92c63212e76e6",
+    "url": "/quoteGenerator/index.html"
+  },
+  {
+    "revision": "d5db6c2f5e3436428683",
+    "url": "/quoteGenerator/static/css/main.0fb2b77e.chunk.css"
+  },
+  {
+    "revision": "a7bc5b07b7e818970fd5",
+    "url": "/quoteGenerator/static/js/2.4316e94e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quoteGenerator/static/js/2.4316e94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5db6c2f5e3436428683",
+    "url": "/quoteGenerator/static/js/main.39a5d703.chunk.js"
+  },
+  {
+    "revision": "a01d0823ed107f4073f0",
+    "url": "/quoteGenerator/static/js/runtime-main.f98dbbf7.js"
+  }
+]);
